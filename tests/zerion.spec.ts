44026fd06497dd0d612bc42e0ceca61c2c774dfa
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-22 15:52:46 +0200

    skip-test

--- tests/zerion.spec.ts
+++ tests/zerion.spec.ts
@@ -3,7 +3,7 @@ import { ZerionIconFeed } from "@/src/lib/icons/zerion";
 import { TORN_MAINNET, TRUMP_BASE } from "./fixtures";
 
 describe("Zerion Icon Archive", () => {
-  it("should retrieve and archive token icons", async () => {
+  it.skip("should retrieve and archive token icons", async () => {
     const zerion = new ZerionIconFeed(getZerionKey(), true);
     const icon = await zerion.getIcon(TORN_MAINNET);
     console.log(icon);
